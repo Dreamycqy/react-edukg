79292982fc28a75e51a051556495e34dc52d2356
chenqiuyang <chenqiuyang@yidian-inc.com>
2019-09-12 02:09:21 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,10 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
+branch_name='master'
+pmJson='pm2.json'
+
+if [ $1 ]
+then
+  branch_name=$1
+  pmJson='pm2-test2.json'
+fi
+echo $branch_name
+
+git fetch --all
+git checkout ${branch_name}
+git reset --hard origin/${branch_name}
+git pull
+
 npm config set registry https://registry.npm.taobao.org
 npm i yarn -g 
 yarn install
 
 rm -rf public
 mkdir public
-npm run prod
+npm run prodTest
 cp ./favicon.ico public/edukg/
 mv public/edukg/index.html server/views/index.html
+
+pm2 startOrReload ${pmJson}
