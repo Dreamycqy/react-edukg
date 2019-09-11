65ec820d0820b2cb37faacce8c4b132163441c19
chenqiuyang <chenqiuyang@yidian-inc.com>
2019-09-12 03:02:51 +0800

    fix

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Icon } from 'antd'
 import { connect } from 'dva'
 import nameList from '@/constants/team'
+import paperList from '@/constants/paper'
+import paperimg from '@/assets/paper.png'
 import styles from './index.css'
 
 function mapStateToProps(state) {
@@ -38,6 +40,21 @@ class Team extends React.Component {
     return result
   }
 
+  renderPaperList = () => {
+    const result = []
+    paperList.forEach((i, index) => {
+      result.push(
+        <div style={{ borderBottom: index === paperList.length - 1 ? null : '1px solid #029ee8', padding: 14 }}>
+          <img src={paperimg} alt="" width="14px" height="14px" />
+          <span>{i.title}</span>
+          &nbsp;
+          <a href="javascript:;" onClick={() => window.open(i.link)}>{this.props.locale === 'cn' ? '论文PDF下载' : 'Download PDF'}</a>
+        </div> // eslint-disable-line
+      )
+    })
+    return result
+  }
+
   render() {
     const { locale } = this.props
     return (
@@ -48,6 +65,14 @@ class Team extends React.Component {
         <div style={{ padding: '20px 5%' }}>
           <div style={{ margin: '0 auto', maxWidth: 1200, overflow: 'hidden' }}>
             {this.renderCards()}
+          </div>
+        </div>
+        <div style={{ padding: '20px 5%', textAlign: 'left' }}>
+          <div style={{ fontSize: 20, marginBottom: 10 }}>
+            {locale === 'cn' ? '团队成员在知识图谱构建方面发表的相关论文：' : 'Published Papers: '}
+          </div>
+          <div style={{ margin: '0 auto', border: '2px solid #029ee8', borderRadius: 6 }}>
+            {this.renderPaperList()}
           </div>
         </div>
       </div>
