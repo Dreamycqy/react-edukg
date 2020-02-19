c4caf1c2f043b5947328c3a9250c8b4e582bd391
陈秋阳 <chenqiuyang@chenqiuyangdeMacBook-Pro.local>
2020-02-19 19:22:22 +0800

    fix

--- server/config.js
+++ server/config.js
@@ -4,17 +4,17 @@ const config = {
   dev: {
     env: 'development',
     port: '8567',
-    basePath: 'http://edukg.org',
+    basePath: 'http://edukg.cn',
   },
   test: {
     env: 'test',
     port: '8567',
-    basePath: 'http://edukg.org',
+    basePath: 'http://edukg.cn',
   },
   production: {
     env: 'production',
     port: '8567',
-    basePath: 'http://edukg.org',
+    basePath: 'http://edukg.cn',
   },
 }
 module.exports = config[process.env.NODE_ENV || 'development']
